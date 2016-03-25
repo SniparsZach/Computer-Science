02a202072df3314d8ab6d048cd3b5c8c27731457
sniparszach <zhui.sh@mysummitps.org>
2016-03-25 18:37:24 +0000

    Slight change in Info Javascript and Added Vindinium bot page

--- info.js
+++ info.js
@@ -13,25 +13,25 @@ var info6 = document.getElementById("info6");
 
 
 item1.addEventListener("click", function(){
-    info1.innerHTML = "<center>'Powerful and reliable, the AK-47 is one of the most popular assault rifles in the world. It is most deadly in short, controlled bursts of fire.' -Official description</center>";
+    info1.innerHTML = "<center>'Powerful and reliable, the AK-47 is one of the most popular assault rifles in the world. It is most deadly in short, controlled bursts of fire.'</center>";
 });
                        
 item2.addEventListener("click", function(){
-    info2.innerHTML = "<center>'A modern homage to a falchion sword, this clip point blade has a curved edge.' -Official description</center>";
+    info2.innerHTML = "<center>'A modern homage to a falchion sword, this clip point blade has a curved edge.'</center>";
 });
                        
 item3.addEventListener("click", function(){
-    info3.innerHTML = "<center>'An ideal pistol for the Terrorist on the move, the Tec-9 is lethal in close quarters and features a high magazine capacity.' -Official description</center>";
+    info3.innerHTML = "<center>'An ideal pistol for the Terrorist on the move, the Tec-9 is lethal in close quarters and features a high magazine capacity.'</center>";
 });
                        
 item4.addEventListener("click", function(){
-    info4.innerHTML = "<center>'High risk and high reward, the infamous AWP is recognizable by its signature report and one-shot, one-kill policy.' -Official description</center>";
+    info4.innerHTML = "<center>'High risk and high reward, the infamous AWP is recognizable by its signature report and one-shot, one-kill policy.'</center>";
 });
                        
 item5.addEventListener("click", function(){
-    info5.innerHTML = "<center>'With its curved blade mimicking a tiger's claw, the karambit was developed as part of the southeast Asian martial discipline of silat. The knife is typically used with a reverse grip, with the finger ring on the index finger.' -Official description</center>";
+    info5.innerHTML = "<center>'With its curved blade mimicking a tiger's claw, the karambit was developed as part of the southeast Asian martial discipline of silat. The knife is typically used with a reverse grip, with the finger ring on the index finger.'</center>";
 });
                        
 item6.addEventListener("click", function(){
-    info6.innerHTML = "<center>'More accurate but less damaging than its AK-47 counterpart, the M4A4 is the full-auto assault rifle of choice for CTs.' -Official description</center>";
+    info6.innerHTML = "<center>'More accurate but less damaging than its AK-47 counterpart, the M4A4 is the full-auto assault rifle of choice for CTs.'</center>";
 });
