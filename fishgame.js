3fbffb92ada5c250701f52eb4975a10bc8ae8f4b
sniparszach <zhui.sh@mysummitps.org>
2016-03-25 01:55:59 +0000

    Finished Fish game javascript [collisions]

--- fishgame.js
+++ fishgame.js
@@ -1,13 +1,142 @@
-window.onload = function() {
-var canvas=document.getElementById("myCanvas"); // grabs the canvas element
-var context=canvas.getContext("2d"); // returns the 2d context object
-var img=new Image(); //creates a variable for a new image
-img.src= "pictures/fishgameboat.png"; // specifies the location of the image
-context.drawImage(img,0,0); // draws the image at the specified x and y location
-}
+var myCanvas = document.getElementById("myCanvas");
+var fishs = [];
+var pole = [];
+var ctx = myCanvas.getContext("2d");
 
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
+var img = document.getElementById("fishgameboat");
+
 ctx.moveTo(0,120);
 ctx.lineTo(700,120);
 ctx.stroke();
+ctx.drawImage(img, 200, 55, 210.740741, 90);
+
+function Fish(xPos, yPos) {
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.height = 12;
+    this.width = 15;
+    ctx.fillstyle = "#FFA500";
+    this.draw = function() {
+        ctx.rect(this.xPos, this.yPos, this.width, this.height);
+        ctx.stroke();
+    };
+    
+    this.move = function() {
+        if(this.yPos < 119) {
+            
+        } else {
+            this.xPos -= -1;
+            this.yPos -= 0;           
+        }
+
+    };
+}
+
+var wave1 = setInterval(function(){
+    
+    var tempRand = myCanvas.height / 3 + Math.random() *  myCanvas.height/3*2;
+    fishs.push(new Fish(0, tempRand));
+
+}, 1500);
+
+var box = {
+    xPos: 270,
+    yPos: 125,
+    goLeft: false,
+    goRight: false,
+    goUp: true,
+    goDown: true,
+    move: function(){
+
+        if(box.goUp && box.yPos > 120){
+            this.yPos -= 5;    
+        }
+        if(box.goDown && box.yPos < 340){
+            this.yPos += 5;    
+        }
+        console.log(this.xPos);
+    },
+    draw: function(){
+        ctx.rect(box.xPos,box.yPos,10,10);
+        ctx.fillRect(box.xPos,box.yPos ,10,10);
+        ctx.stroke();
+    }
+}
+
+
+document.addEventListener("keydown", function(evt){
+    if(evt.keyCode === 37){
+        box.goLeft = true;
+    }
+    if(evt.keyCode === 38){
+        box.goUp = true;
+    }
+    if(evt.keyCode === 39){
+        box.goRight = true;
+    }
+    if(evt.keyCode === 40){
+        box.goDown = true;        
+    }    
+    
+});
+
+document.addEventListener("keyup", function(evt){
+    if(evt.keyCode === 37){
+        box.goLeft = false;
+    }
+    if(evt.keyCode === 38){
+        box.goUp = false;
+    }
+    if(evt.keyCode === 39){
+        box.goRight = false;
+    }
+    if(evt.keyCode === 40){
+        box.goDown = false;        
+    }    
+})
+
+function gameLoop() {
+    ctx.beginPath();
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    // Draw Background Colors
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(0,0,700,400);
+    ctx.fillStyle = "#668FFF";
+    ctx.fillRect(0,120,700,280);
+    // Draw Boat
+    ctx.drawImage(img, 280, 55, 210.740741, 90);
+    
+    ctx.fillStyle = "#000000";
+    box.move();
+    box.draw();
+    
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.moveTo(300,100);
+    ctx.lineTo(275,70);
+    ctx.lineTo(275, box.yPos);
+    ctx.stroke();
+
+    for (var i = 0; i < fishs.length; i++) {
+        fishs[i].move();
+        fishs[i].draw();
+        if(isColliding(fishs[i],box)){
+            fishs.splice(i,1);
+        }
+    }
+
+    window.requestAnimationFrame(gameLoop)
+}
+
+function isColliding(box, fishs){
+    console.log(box, fishs);
+    var isLeft = fishs.xPos + fishs.width < box.xPos;
+    var isRight = fishs.xPos > box.xPos + box.width;
+    var isBelow = fishs.yPos + fishs.height < box.yPos;
+    var isAbove = fishs.yPos > box.yPos + box.height;
+    return !(isRight||isLeft||isAbove||isBelow);
+}
+
+gameLoop();
--- info.js
+++ info.js
@@ -2,13 +2,36 @@ var item1 = document.getElementById("item1");
 var item2 = document.getElementById("item2");
 var info1 = document.getElementById("info1");
 var info2 = document.getElementById("info2");
+var item3 = document.getElementById("item3");
+var item4 = document.getElementById("item4");
+var info3 = document.getElementById("info3");
+var info4 = document.getElementById("info4");
+var item5 = document.getElementById("item5");
+var item6 = document.getElementById("item6");
+var info5 = document.getElementById("info5");
+var info6 = document.getElementById("info6");
+
 
 item1.addEventListener("click", function(){
-    info1.innerHTML = "Some stuff";
-    info2.innerHTML = "some other stuff";
+    info1.innerHTML = "<center>test</center>";
 });
                        
 item2.addEventListener("click", function(){
-    info1.innerHTML = "Item 2 stuff";
-    info2.innerHTML = "Item 2 other stuff";
+    info2.innerHTML = "<center>test</center>";
+});
+                       
+item3.addEventListener("click", function(){
+    info3.innerHTML = "<center>test</center>";
+});
+                       
+item4.addEventListener("click", function(){
+    info4.innerHTML = "<center>test</center>";
+});
+                       
+item5.addEventListener("click", function(){
+    info5.innerHTML = "<center>test</center>";
+});
+                       
+item6.addEventListener("click", function(){
+    info6.innerHTML = "<center>test</center>";
 });
