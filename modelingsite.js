b1905374ec3d8fc3f214aad809775b8ab8ee9c9e
sniparszach <zhui.sh@mysummitps.org>
2016-03-31 00:12:07 +0000

    Added Modeling page, still needs more buttons for javascript

---
+++ modelingsite.js
@@ -0,0 +1,95 @@
+var ddussr = document.getElementById("ddussr");
+var ddussri = document.getElementById("ddussri");
+var ddusa = document.getElementById("ddusa");
+var ddusai = document.getElementById("ddusai");
+var ddjapan = document.getElementById("ddjapan");
+var ddjapani = document.getElementById("ddjapani");
+
+var crusa = document.getElementById("crusa");
+var crusai = document.getElementById("crusai");
+var crussr = document.getElementById("crussr");
+var crussri = document.getElementById("crussri");
+var crjapan = document.getElementById("crjapan");
+var crjapani = document.getElementById("crjapani");
+var crgerman = document.getElementById("crgerman");
+var crgermani = document.getElementById("crgermani");
+
+var bbmontana = document.getElementById("bbmontana");
+var bbmontanai = document.getElementById("bbmontanai");
+var bbyamato = document.getElementById("bbyamato");
+var bbyamatoi = document.getElementById("bbyamatoi");
+
+var acmidway = document.getElementById("acmidway");
+var acmidwayi = document.getElementById("acmidwayi");
+var achakuryu = document.getElementById("achakuryu");
+var achakuryui = document.getElementById("achakuryui");
+
+var info11 = document.getElementById("info11");
+var info12 = document.getElementById("info14");
+
+var item13 = document.getElementById("item13");
+var item14 = document.getElementById("item14");
+
+var info13 = document.getElementById("info13");
+var info14 = document.getElementById("info14");
+
+ddussr.addEventListener("click", function(){
+    ddussri.innerHTML = "<center><p>The ship was designed as a destroyer flotilla leader. No destroyer in the world could rival her in terms of speed, while her armor and the power of her artillery were comparable to those of light cruisers. Dual-purpose main guns and a battery of automatic AA guns ensured a strong AA defense.</p></center>";
+});
+
+ddusa.addEventListener("click", function(){
+    ddusai.innerHTML = "<center><p>Developed from the Fletcher class, Gearing was the largest destroyer in the U.S. Navy during World War II. The ship was superior in firepower to all foreign counterparts, owing to new dual-purpose twin mounts with a very high rate of fire. Numerous automatic AA guns ensured a respective level of efficiency for her AA defenses.</p></center>";
+});
+                       
+ddjapan.addEventListener("click", function(){
+    ddjapani.innerHTML = "<center><p>An experimental 'cruiser type' destroyer with increased displacement. The ship was equipped with very powerful propulsion, making Shimakaze faster than the majority of her contemporary ships of the same type. Shimakaze was second to none in terms of torpedo armament. The first and only destroyer of the Imperial Japanese Navy to be equipped with quintuple torpedo tubes.</p></center>";
+});
+                       
+crusa.addEventListener("click", function(){
+    crusai.innerHTML = "<center><p>One of the best heavy cruisers in the world. The ship was designed using the experience gained in World War II, resulting in excellent protection and very powerful AA armament. The key distinctive feature of cruisers of this type was auto-loading main guns that had a very high rate of fire.</p></center>";
+});
+                       
+crussr.addEventListener("click", function(){
+    crussri.innerHTML = "<center><p>A ship designed and intended to destroy light enemy cruisers and fight against heavy cruisers (Project 66). Unlike her foreign counterparts, she boasted bigger dimensions and better armor protection. The cruiser's main battery surpassed foreign guns in maximum range and destructive power but had a lower rate of fire.</p></center>";
+});
+                       
+crjapan.addEventListener("click", function(){
+    crjapani.innerHTML = "<center><p>A very powerful cruiser in all respects. Compared with the preceding classes of heavy cruisers, Zao had a greater number of main battery and AA guns, thick armor and a reinforced torpedo armament.</p></center>";
+});
+
+crgerman.addEventListener("click", function(){
+    crgermani.innerHTML = "<center><p>The ship was developed from a series of German heavy cruisers. Unlike ships of preceding classes, she boasted an increased displacement and more powerful main battery guns.</p></center>";
+});
+
+bbmontana.addEventListener("click", function(){
+    bbmontanai.innerHTML = "<center><p>One of the largest battleships in the world. She was slightly inferior to Iowa-class ships in terms of speed, but had more main guns and significantly stronger torpedo protection. Montana's armor was conventionally arranged and considerably reinforced.</p></center>";
+});
+
+bbyamato.addEventListener("click", function(){
+    bbyamatoi.innerHTML = "<center><p>The biggest warship of World War II and the world's largest battleship. Yamato was designed around the idea that an individual ship could have superiority over any battleship of a potential enemy. Her main guns had overwhelming firepower. The ship maintained a very high level of survivability due to reliable armor and robust torpedo protection. Yamato's AA capabilities were highly efficient due to carrying numerous AA artillery guns.</p></center>";
+});
+
+acmidway.addEventListener("click", function(){
+    acmidwayi.innerHTML = "<center><p>The largest U.S. Navy warship laid down during World War II. The huge size of this aircraft carrier allowed her to carry the largest air group, compared to her contemporary counterparts. Excellent torpedo protection, well-armored flight deck and powerful AA defenses made Midway-class ships virtually invulnerable to attacks from enemy aircraft.</p></center>";
+});
+
+achakuryu.addEventListener("click", function(){
+    achakuryui.innerHTML = "<center><p>An aircraft carrier designed to be an enlarged and enhanced version of the Taiho class. Hakuryu was superior to her predecessor in size, armor and artillery power. Owing to an increased hangar, she could carry the largest air group among all Japanese aircraft carriers.</p></center>";
+});
+
+item11.addEventListener("click", function(){
+    info11.innerHTML = "<center><p>test</p></center>";
+});
+
+item12.addEventListener("click", function(){
+    info12.innerHTML = "<center><p>test</p></center>";
+});
+
+item13.addEventListener("click", function(){
+    info13.innerHTML = "<center><p>test</p></center>";
+});
+
+item14.addEventListener("click", function(){
+    info14.innerHTML = "<center><p>test</p></center>";
+});
+
