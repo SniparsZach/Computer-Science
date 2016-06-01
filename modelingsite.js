357e59e1a7e6658285c8a4df7be9f37e29c9b5ff
sniparszach <zhui.sh@mysummitps.org>
2016-06-01 22:11:43 +0000

    Finished modeling page, updated hardware page, cleaned up pictures folder.

--- modelingsite.js
+++ modelingsite.js
@@ -1,95 +1,63 @@
-var ddussr = document.getElementById("ddussr");
-var ddussri = document.getElementById("ddussri");
-var ddusa = document.getElementById("ddusa");
-var ddusai = document.getElementById("ddusai");
-var ddjapan = document.getElementById("ddjapan");
-var ddjapani = document.getElementById("ddjapani");
+// JavaScript File
 
-var crusa = document.getElementById("crusa");
-var crusai = document.getElementById("crusai");
-var crussr = document.getElementById("crussr");
-var crussri = document.getElementById("crussri");
-var crjapan = document.getElementById("crjapan");
-var crjapani = document.getElementById("crjapani");
-var crgerman = document.getElementById("crgerman");
-var crgermani = document.getElementById("crgermani");
+// These variables contain the html's elements and important arrays that I will use later on in my functions //
+var firstText = document.getElementById("first_text");
+var nextButton = document.getElementById("nextbutton");
+var backButton = document.getElementById("backbutton");
+var image = document.getElementById("image");
+var heading = document.getElementById("heading");
+var info = document.getElementById("info");
+var imageArray = ["pictures/ships/japanese/hakuryu.png" , "pictures/ships/japanese/yamato.png","pictures/ships/japanese/zao.png","pictures/ships/japanese/shimakaze.png","pictures/ships/usa/midway.png","pictures/ships/usa/montana.png","pictures/ships/usa/desmoines.png","pictures/ships/usa/gearing.png","pictures/ships/ussr/moskva.png","pictures/ships/ussr/khabarovsk.png"];
+var headingsArray = ["Japan - CV - Hakaryu" , "Japan - BB - Yamato","Japan - CA - Zao" , "Japan - DD - Shimakaze","USA - CV - Midway","USA - BB - Montana","USA - CA - Des Moines","USA - DD - Gearing","USSR - CA - Moskva","USSR - DD - Khabarovsk"];
+var infoArray = ["An aircraft carrier designed to be an enlarged and enhanced version of the Taiho class. Hakuryu was superior to her predecessor in size, armor and artillery power. Owing to an increased hangar, she could carry the largest air group among all Japanese aircraft carriers.","The biggest warship of World War II and the world’s largest battleship. Yamato was designed around the idea that an individual ship could have superiority over any battleship of a potential enemy. Her main guns had overwhelming firepower. The ship maintained a very high level of survivability due to reliable armor and robust torpedo protection. Yamato’s AA capabilities were highly efficient due to carrying numerous AA artillery guns.","A very powerful cruiser in all respects. Compared with the preceding classes of heavy cruisers, Zao had a greater number of main battery and AA guns, thick armor and a reinforced torpedo armament.","An experimental cruiser type destroyer with increased displacement. The ship was equipped with very powerful propulsion, making Shimakaze faster than the majority of her contemporary ships of the same type. Shimakaze was second to none in terms of torpedo armament. The first and only destroyer of the Imperial Japanese Navy to be equipped with quintuple torpedo tubes.","The largest U.S. Navy warship laid down during World War II. The huge size of this aircraft carrier allowed her to carry the largest air group, compared to her contemporary counterparts. Excellent torpedo protection, well-armored flight deck and powerful AA defenses made Midway-class ships virtually invulnerable to attacks from enemy aircraft.","One of the largest battleships in the world. She was slightly inferior to Iowa-class ships in terms of speed, but had more main guns and significantly stronger torpedo protection. Montana’s armor was conventionally arranged and considerably reinforced.","One of the best heavy cruisers in the world. The ship was designed using the experience gained in World War II, resulting in excellent protection and very powerful AA armament. The key distinctive feature of cruisers of this type was auto-loading main guns that had a very high rate of fire.","Developed from the Fletcher class, Gearing was the largest destroyer in the U.S. Navy during World War II. The ship was superior in firepower to all foreign counterparts, owing to new dual-purpose twin mounts with a very high rate of fire. Numerous automatic AA guns ensured a respective level of efficiency for her AA defenses.","A ship designed and intended to destroy light enemy cruisers and fight against heavy cruisers (Project 66). Unlike her foreign counterparts, she boasted bigger dimensions and better armor protection. The cruiser’s main battery surpassed foreign guns in maximum range and destructive power but had a lower rate of fire.","The ship was designed as a destroyer flotilla leader. No destroyer in the world could rival her in terms of speed, while her armor and the power of her artillery were comparable to those of light cruisers. Dual-purpose main guns and a battery of automatic AA guns ensured a strong AA defense."];
 
-var bbmontana = document.getElementById("bbmontana");
-var bbmontanai = document.getElementById("bbmontanai");
-var bbyamato = document.getElementById("bbyamato");
-var bbyamatoi = document.getElementById("bbyamatoi");
+// These variables contain the index of the current ship so they can be increased or decreased with the functions later on. //
+var currentImageIndex = -1;
+var currentHeadingsIndex = -1;
+var currentInfoIndex = -1;
 
-var acmidway = document.getElementById("acmidway");
-var acmidwayi = document.getElementById("acmidwayi");
-var achakuryu = document.getElementById("achakuryu");
-var achakuryui = document.getElementById("achakuryui");
+nextButton.addEventListener("click" , next);
 
-var info11 = document.getElementById("info11");
-var info12 = document.getElementById("info14");
+// This function makes it so when the next button is clicked, the next ship, info, and heading appear. //
+function next() {
+  
+    if (currentImageIndex < imageArray.length-1 && currentHeadingsIndex < headingsArray.length-1 && currentInfoIndex < infoArray.length-1) {
+        
+        currentImageIndex++;
+        currentHeadingsIndex++;
+        currentInfoIndex++;
+        
+    image.src = imageArray[currentImageIndex];
+    heading.innerHTML = headingsArray[currentHeadingsIndex];
+    info.innerHTML = infoArray[currentInfoIndex];
+    
+    }
+    
+    firstText.innerHTML = "";
+    
+}
 
-var item13 = document.getElementById("item13");
-var item14 = document.getElementById("item14");
+backButton.addEventListener("click" , previous);
 
-var info13 = document.getElementById("info13");
-var info14 = document.getElementById("info14");
+// This function makes it so when the previous button is clicked, the previous ship, info, and heading appear. //
+function previous() {
+    
+    if (currentImageIndex >= 1 && currentHeadingsIndex >= 1 && currentInfoIndex >= 1) {
+        
+    currentImageIndex--;
+    currentHeadingsIndex--;
+    currentInfoIndex--;
 
-ddussr.addEventListener("click", function(){
-    ddussri.innerHTML = "<center><p>The ship was designed as a destroyer flotilla leader. No destroyer in the world could rival her in terms of speed, while her armor and the power of her artillery were comparable to those of light cruisers. Dual-purpose main guns and a battery of automatic AA guns ensured a strong AA defense.</p></center>";
-});
+    image.src = imageArray[currentImageIndex];
+    heading.innerHTML = headingsArray[currentHeadingsIndex];
+    info.innerHTML = infoArray[currentInfoIndex];
+    
+    }
+    
+}
 
-ddusa.addEventListener("click", function(){
-    ddusai.innerHTML = "<center><p>Developed from the Fletcher class, Gearing was the largest destroyer in the U.S. Navy during World War II. The ship was superior in firepower to all foreign counterparts, owing to new dual-purpose twin mounts with a very high rate of fire. Numerous automatic AA guns ensured a respective level of efficiency for her AA defenses.</p></center>";
-});
-                       
-ddjapan.addEventListener("click", function(){
-    ddjapani.innerHTML = "<center><p>An experimental 'cruiser type' destroyer with increased displacement. The ship was equipped with very powerful propulsion, making Shimakaze faster than the majority of her contemporary ships of the same type. Shimakaze was second to none in terms of torpedo armament. The first and only destroyer of the Imperial Japanese Navy to be equipped with quintuple torpedo tubes.</p></center>";
-});
-                       
-crusa.addEventListener("click", function(){
-    crusai.innerHTML = "<center><p>One of the best heavy cruisers in the world. The ship was designed using the experience gained in World War II, resulting in excellent protection and very powerful AA armament. The key distinctive feature of cruisers of this type was auto-loading main guns that had a very high rate of fire.</p></center>";
-});
-                       
-crussr.addEventListener("click", function(){
-    crussri.innerHTML = "<center><p>A ship designed and intended to destroy light enemy cruisers and fight against heavy cruisers (Project 66). Unlike her foreign counterparts, she boasted bigger dimensions and better armor protection. The cruiser's main battery surpassed foreign guns in maximum range and destructive power but had a lower rate of fire.</p></center>";
-});
-                       
-crjapan.addEventListener("click", function(){
-    crjapani.innerHTML = "<center><p>A very powerful cruiser in all respects. Compared with the preceding classes of heavy cruisers, Zao had a greater number of main battery and AA guns, thick armor and a reinforced torpedo armament.</p></center>";
-});
 
-crgerman.addEventListener("click", function(){
-    crgermani.innerHTML = "<center><p>The ship was developed from a series of German heavy cruisers. Unlike ships of preceding classes, she boasted an increased displacement and more powerful main battery guns.</p></center>";
-});
 
-bbmontana.addEventListener("click", function(){
-    bbmontanai.innerHTML = "<center><p>One of the largest battleships in the world. She was slightly inferior to Iowa-class ships in terms of speed, but had more main guns and significantly stronger torpedo protection. Montana's armor was conventionally arranged and considerably reinforced.</p></center>";
-});
 
-bbyamato.addEventListener("click", function(){
-    bbyamatoi.innerHTML = "<center><p>The biggest warship of World War II and the world's largest battleship. Yamato was designed around the idea that an individual ship could have superiority over any battleship of a potential enemy. Her main guns had overwhelming firepower. The ship maintained a very high level of survivability due to reliable armor and robust torpedo protection. Yamato's AA capabilities were highly efficient due to carrying numerous AA artillery guns.</p></center>";
-});
 
-acmidway.addEventListener("click", function(){
-    acmidwayi.innerHTML = "<center><p>The largest U.S. Navy warship laid down during World War II. The huge size of this aircraft carrier allowed her to carry the largest air group, compared to her contemporary counterparts. Excellent torpedo protection, well-armored flight deck and powerful AA defenses made Midway-class ships virtually invulnerable to attacks from enemy aircraft.</p></center>";
-});
-
-achakuryu.addEventListener("click", function(){
-    achakuryui.innerHTML = "<center><p>An aircraft carrier designed to be an enlarged and enhanced version of the Taiho class. Hakuryu was superior to her predecessor in size, armor and artillery power. Owing to an increased hangar, she could carry the largest air group among all Japanese aircraft carriers.</p></center>";
-});
-
-item11.addEventListener("click", function(){
-    info11.innerHTML = "<center><p>test</p></center>";
-});
-
-item12.addEventListener("click", function(){
-    info12.innerHTML = "<center><p>test</p></center>";
-});
-
-item13.addEventListener("click", function(){
-    info13.innerHTML = "<center><p>test</p></center>";
-});
-
-item14.addEventListener("click", function(){
-    info14.innerHTML = "<center><p>test</p></center>";
-});
 
